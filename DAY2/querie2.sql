59e22e624495231f672e41dc7708f35d2e864dee
Amank0704 <akshirsagar0704@gmail.com>
2026-06-13 17:04:23 +0530

    10 queries on CATEGORY & PRODUCT TABLE

---
+++ DAY2/querie2.sql
@@ -0,0 +1,14 @@
+-- 11. Display all product categories. 
+SELECT * FROM category;
+
+
+-- 12. List all products with price greater than 150. 
+SELECT * FROM product WHERE price > 150;
+-- 13. Find products priced between 100 and 180. 
+-- 14. Display product name and price of all products. 
+-- 15. Count the number of products in each category. 
+-- 16. Find the maximum and minimum product price. 
+-- 17. Calculate the average price of all products. 
+-- 18. Find products belonging to 'Electronics' or 'Clothing' category. 
+-- 19. Increase the price of product 'Laptop' to 55000. 
+-- 20. List products sorted by product name in ascending order.
