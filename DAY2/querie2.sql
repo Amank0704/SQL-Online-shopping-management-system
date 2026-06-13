22ed2d06e521303dfdcecfccd0f89901fd2cc9e6
Amank0704 <akshirsagar0704@gmail.com>
2026-06-13 17:07:39 +0530

    10 queries on CATEGORY & PRODUCT TABLE

--- DAY2/querie2.sql
+++ DAY2/querie2.sql
@@ -11,8 +11,25 @@ SELECT * FROM product WHERE price BETWEEN 100 AND 180;
 
 
 -- 14. Display product name and price of all products. 
--- 15. Count the number of products in each category. 
--- 16. Find the maximum and minimum product price. 
+SELECT product_name, price FROM product;
+
+
+-- 15. Count the number of products in each category.
+SELECT c.category_name,
+       COUNT(p.product_id) AS total_products
+FROM category c
+JOIN product p
+ON c.category_id = p.category_id
+GROUP BY c.category_name;
+
+
+-- 16. Find the maximum and minimum product price.
+SELECT MAX(price) AS Maximum_Price,
+       MIN(price) AS Minimum_Price
+FROM product;
+
+
+
 -- 17. Calculate the average price of all products. 
 -- 18. Find products belonging to 'Electronics' or 'Clothing' category. 
 -- 19. Increase the price of product 'Laptop' to 55000. 
